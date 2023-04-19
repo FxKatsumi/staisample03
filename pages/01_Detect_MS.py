8cfd746c0c17c30ff09285eefb96979a3a66de4b
fxKatsumi <takahata@forex.co.jp>
2023-04-19 16:48:04 +0900

    ロゴマーク対応改修

--- pages/01_Detect_MS.py
+++ pages/01_Detect_MS.py
@@ -1,12 +1,18 @@
+# 【wallprime】Custom Visionで物体検出
+# https://remix-yh.net/1156/
+
+# Custom Vision 使用する画像内の物体の検出
+# https://microsoftlearning.github.io/AI-102JA-Designing-and-Implementing-a-Microsoft-Azure-AI-Solution/Instructions/18-object-detection.html
+
 from common.detectmain import appmain
 
 # ＜Azure＞
 from common.azure import VisionAPI
 
 # 精度しきい値
-probability_threshold = 0.9
+probability_threshold = 0.92
 
 # プログラム開始
 if __name__ == "__main__":
     # メイン処理
-    appmain("Microsoft", "バナナ、リンゴ、オレンジのみを学習したモデル", VisionAPI, probability_threshold)
+    appmain("Microsoft", "フォレックス ロゴマークのみを学習したモデル", VisionAPI, probability_threshold)
